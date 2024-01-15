c44cf307008e1b49911dfc5138909a0d7b6abb09
Benjamin Randolph <neb417@gmail.com>
2024-01-15 10:05:38 -0700

    Test and create a player

---
+++ lib/player.rb
@@ -0,0 +1,15 @@
+class Player
+  attr_reader :name, :deck, :has_lost
+
+  def initialize(name, deck)
+    @name = name
+    @deck = deck
+    @has_lost = false
+  end
+
+  def has_lost?
+    @has_lost = true if deck.cards.empty?
+
+    @has_lost
+  end
+end
